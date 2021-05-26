39d0f2410b7596bcab275d9fa45da3470826878f
santiago <santiago.kenshinvaldez@gmail.com>
2021-05-26 11:11:18 -0400

    Make single new page scrollable

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'ParaguayNews'),
@@ -106,90 +97,91 @@ class _MyHomePageState extends State<MyHomePage>
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text('$_counter', style: Theme.of(context).textTheme.headline4),
-            Text('$_contenido', style: Theme.of(context).textTheme.headline5),
-            Container(
-                decoration:
-                    new BoxDecoration(color: Color.fromARGB(255, 255, 100, 10)),
-                child: ButtonBar(children: <Widget>[
-                  FlatButton(
-                      onPressed: () async {
-                        String url =
-                            'https://jsonplaceholder.typicode.com/albums/$_counter';
-                        debugPrint(url);
-                        final response = await http.get(url);
-                        if (response.statusCode == 200) {
-                          debugPrint(response.body.toString());
-                          _setContenido(response.body);
-                        } else {
-                          throw Exception('Failed to load album');
-                        }
-                      },
-                      child: Text('Olis'),
-                      color: Color.fromARGB(255, 0, 255, 0)),
-                  OutlineButton(
-                      onPressed: () {
-                        return showDialog<void>(
-                          context: context,
-                          barrierDismissible: false, // user must tap button!
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text('Rewind and remember'),
-                              content: SingleChildScrollView(
-                                child: ListBody(
-                                  children: <Widget>[
-                                    Text('You will never be satisfied.'),
-                                    Text(
-                                        'You\’re like me. I’m never satisfied.'),
-                                  ],
-                                ),
-                              ),
-                              actions: <Widget>[
-                                TextButton(
-                                  child: Text('Regret'),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                              ],
-                            );
-                          },
-                        ); /*  */
-                      },
-                      child: Text('Olis'),
-                      color: Color.fromARGB(255, 0, 0, 255)),
-                ])),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10),
-              height: animation.value,
-              width: animation.value,
-              child: FlutterLogo(),
-            ),
-          ],
-        ),
+      body: ListView(
+        shrinkWrap: true,
+        padding: const EdgeInsets.all(20.0),
+        children: const <Widget>[
+          Text("I'm dedicating every day to you"),
+          Text('Domestic life was never quite my style'),
+          Text('When you smile, you knock me out, I fall apart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text("I'm dedicating every day to you"),
+          Text('Domestic life was never quite my style'),
+          Text('When you smile, you knock me out, I fall apart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text("I'm dedicating every day to you"),
+          Text('Domestic life was never quite my style'),
+          Text('When you smile, you knock me out, I fall apart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text("I'm dedicating every day to you"),
+          Text('Domestic life was never quite my style'),
+          Text('When you smile, you knock me out, I fall apart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+          Text('And I thought I was so smart'),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
@@ -213,37 +205,62 @@ class FirstScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('First Screen - !'),
       ),
-      body: Container(
-          child: Column(
-        children: [
-          Row(
-            children: [
-              FittedBox(
-                child: Image.asset(
-                  'images/placeholder.jpg',
-                  width: MediaQuery.of(context).size.width,
+      body: ListView(children: [
+        Column(
+          children: [
+            Row(
+              children: [
+                FittedBox(
+                  child: Image.asset(
+                    'images/placeholder.jpg',
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                  fit: BoxFit.fill,
                 ),
-                fit: BoxFit.fill,
+              ],
+            ),
+            Container(
+              child: Text(
+                'Titulo de Noticia - Este titulo es bastante largo veamos que tal se ve esta verga',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
               ),
-            ],
-          ),
-          Container(
-            child: Text(
-              'Titulo de Noticia - Este titulo es bastante largo veamos que tal se ve esta verga',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              margin: const EdgeInsets.all(12.0),
             ),
-            margin: const EdgeInsets.all(12.0),
-          ),
-          Container(
-            child: Text(
-              'Esto es un texto multilinea. Donde se supone que esto se va a renderear de otra forma. Nunca sabermos si esto funca o no.\n\nO si es alguna cosa rara del puto dart.\n\nEn otras noticias....',
-              textAlign: TextAlign.justify,
+            Container(
+              child: Column(
+                children: [
+                  Text(
+                    'Esto es un texto multilinea. Donde se supone que esto se va a renderear de otra forma. Nunca sabermos si esto funca o no.\n\nO si es alguna cosa rara del puto dart.\n\nEn otras noticias....',
+                    textAlign: TextAlign.justify,
+                  ),
+                  Text(
+                    'Esto es un texto multilinea. Donde se supone que esto se va a renderear de otra forma. Nunca sabermos si esto funca o no.\n\nO si es alguna cosa rara del puto dart.\n\nEn otras noticias....',
+                    textAlign: TextAlign.justify,
+                  ),
+                  Text(
+                    'Esto es un texto multilinea. Donde se supone que esto se va a renderear de otra forma. Nunca sabermos si esto funca o no.\n\nO si es alguna cosa rara del puto dart.\n\nEn otras noticias....',
+                    textAlign: TextAlign.justify,
+                  ),
+                  Text(
+                    'Esto es un texto multilinea. Donde se supone que esto se va a renderear de otra forma. Nunca sabermos si esto funca o no.\n\nO si es alguna cosa rara del puto dart.\n\nEn otras noticias....',
+                    textAlign: TextAlign.justify,
+                  ),
+                  Text(
+                    'Esto es un texto multilinea. Donde se supone que esto se va a renderear de otra forma. Nunca sabermos si esto funca o no.\n\nO si es alguna cosa rara del puto dart.\n\nEn otras noticias....',
+                    textAlign: TextAlign.justify,
+                  ),
+                  Text(
+                    'Esto es un texto multilinea. Donde se supone que esto se va a renderear de otra forma. Nunca sabermos si esto funca o no.\n\nO si es alguna cosa rara del puto dart.\n\nEn otras noticias....',
+                    textAlign: TextAlign.justify,
+                  ),
+                ],
+              ),
+              margin: const EdgeInsets.all(12.0),
             ),
-            margin: const EdgeInsets.all(12.0),
-          ),
-        ],
-      )),
+          ],
+        )
+      ]),
     );
   }
 }
